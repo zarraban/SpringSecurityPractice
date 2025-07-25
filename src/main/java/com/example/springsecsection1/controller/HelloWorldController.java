7f2d58bf8117aedf43dc2e10f819c5a1ac424446
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-25 16:54:11 +0300

    Add HelloWorldController

---
+++ src/main/java/com/example/springsecsection1/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String getGreeting(){
+        return "Hello World!";
+    }
+}
