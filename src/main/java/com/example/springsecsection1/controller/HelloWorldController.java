e8b36f8601ffc315f8ed8f630f91ec665dc51d72
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-31 21:33:29 +0300

    Change return entity of HelloController

--- src/main/java/com/example/springsecsection1/controller/HelloWorldController.java
+++ src/main/java/com/example/springsecsection1/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.springsecsection1.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String getGreeting(){
-        return "Hello World!";
+    public ResponseEntity<String> getGreeting(){
+        return new ResponseEntity<>("Hello World",HttpStatus.OK);
     }
 }
