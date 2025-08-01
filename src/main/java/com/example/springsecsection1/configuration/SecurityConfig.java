62dfe2cc15fdc2198edb00579a51aa66e29ea2b6
Smyrnov Vlad <vladsimovv@gmail.com>
2025-08-01 19:47:47 +0300

    Add two Users to INMemoryUserDetails

--- src/main/java/com/example/springsecsection1/configuration/SecurityConfig.java
+++ src/main/java/com/example/springsecsection1/configuration/SecurityConfig.java
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -13,10 +19,20 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> (requests.anyRequest()).permitAll());
-        http.formLogin(flc->flc.disable());
-        http.httpBasic(hbc->hbc.disable());
+        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("vlad1").password("{noop}1234").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1234").authorities("admin").build();
+        return new InMemoryUserDetailsManager(
+                user,
+                admin
+        );
     }
 
 
--- src/main/java/com/example/springsecsection1/controller/HelloWorldController.java
+++ src/main/java/com/example/springsecsection1/controller/HelloWorldController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> getGreeting(){
-        return new ResponseEntity<>("Hello World",HttpStatus.OK);
+    public String getGreeting(){
+        return "hello";
+//        return new ResponseEntity<>("Hello World",HttpStatus.OK);
     }
 }
